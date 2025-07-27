9c70ec3523e901aaabff3519345b8c41a4fbf753
Anshul <anshulraturi007@gmail.com>
2025-07-27 17:32:30 +0530

    add chat room

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
+import ChatRoom from "./components/ChatRoom.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
+        <Route path='/room/:roomId' element={<ChatRoom />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
